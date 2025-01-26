b65c33b701ffbcebea0071a1346b07e55755a494
Connor <langley.conn@gmail.com>
2025-01-26 15:52:47 -0600

    Add basic nav bar with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ResumePageComponent } from './components/resume-page/resume-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomePageComponent},
+    {path: 'resume', component: ResumePageComponent},
+    {path: 'contact', component: ContactPageComponent}
+];
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  constructor(private _router: Router){}
+
+  public routeTo(route : string){
+    this._router.navigate([route]);
+  }
+
+  public openGitHub(){
+    const url = "https://github.com/langleyCon/projects"
+    window.open(url, '_blank')
+  }
 
 }
