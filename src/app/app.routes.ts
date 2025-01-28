9c1bb591603d98fff552fe68143f8b06c105a95e
Connor <langley.conn@gmail.com>
2025-01-27 19:36:41 -0600

    add about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { ResumePageComponent } from './components/resume-page/resume-page.component';
 import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 export const routes: Routes = [
     {path: '', component: HomePageComponent},
     {path: 'resume', component: ResumePageComponent},
-    {path: 'contact', component: ContactPageComponent}
+    {path: 'contact', component: ContactPageComponent},
+    {path: 'about', component: AboutMeComponent}
 ];
