b6685776dc622ea74624f1b495efe85b79ff60f9
Connor <langley.conn@gmail.com>
2025-01-29 20:41:37 -0600

    Add content to contact page

--- src/app/components/contact-page/contact-page.component.ts
+++ src/app/components/contact-page/contact-page.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './contact-page.component.scss'
 })
 export class ContactPageComponent {
+  public email = "langley.conn@gmail.com";
+
+  public openLinkedIn() : void {
+    const url = "https://www.linkedin.com/in/connor-langley-uwl2020";
+    window.open(url, '_blank');
+  }
 
 }
