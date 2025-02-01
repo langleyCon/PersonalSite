5ab57e58c04e7b150f103da7c120cb453020f022
Connor <langley.conn@gmail.com>
2025-02-01 14:54:53 -0600

    add content and slideshow to about page

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal} from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
@@ -6,6 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit{
+
+  private currentIndex = signal(0);
+  private images : string[] = ['Lady', 'Cats', 'boarding'];
+
+  public currentPhoto = signal(this.images[this.currentIndex()]);
+
+  ngOnInit(): void {
+    this.startSlideShow();
+  }
+
+  private startSlideShow(): void {
+    setInterval(() => {
+      this.nextPhoto();
+    }, 5000);
+  }
+
+  private nextPhoto() : void {
+    this.currentIndex.update(idx => idx == this.images.length - 1 ? 0 : idx + 1);
+    this.currentPhoto.update(photo => photo = this.images[this.currentIndex()]);
+  }
+
 
 }
